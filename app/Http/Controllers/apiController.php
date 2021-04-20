0a8d7c83cb1dbfab1da4c36aa102dd4c2f913eb5
sandrojrs <sandrojuniorsilva@hotmail.com>
2021-04-19 23:23:34 -0300

    create api cotação

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class apiController extends Controller
 {
-    //
+    public function apiCoin($tipo,$coin)
+    {
+        $client = new Client();
+        $url = "https://economia.awesomeapi.com.br/".$tipo."/".$coin;
+
+        $response = $client->request('GET', $url, [
+            'verify'  => false,
+        ]);
+        $responseBody = json_decode($response->getBody());
+        dd($responseBody);
+
+       // return view('projects.apiwithoutkey', compact('responseBody'));
+    }
 }
